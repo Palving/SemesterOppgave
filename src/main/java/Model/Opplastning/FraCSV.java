32a5ab727b9897e920e2a8e66d0e192dab21b73d
jonraf <jonmrafoss@gmail.com>
2019-05-03 13:30:47 +0200

    oppu

--- src/main/java/Model/Opplastning/FraCSV.java
+++ src/main/java/Model/Opplastning/FraCSV.java
@@ -5,15 +5,20 @@
  */
 package Model.Opplastning;
 
+import Model.Domene.Arrangement;
 import Model.Domene.Artist;
 import Model.Domene.Billett;
 import Model.Domene.KontaktPerson;
 import Model.Domene.Lokale;
 import Model.Registrering.Register;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,58 +26,84 @@ import java.util.Arrays;
  *
  * @author jonny
  */
-public class FraCSV extends OpplastingSystem {
-    private String vei;
-    private Register register;
-    public FraCSV (File file){
+public class FraCSV extends OpplastingSystem{
+    private File file;
+    
+    public FraCSV(File file){
         super(file);
     }
     
-    
-     public KontaktPerson finnKontaktPerson(ArrayList<String> array3){
+    public KontaktPerson finnKontaktPerson(ArrayList<String> linjeLestInn){
+        Register register = Register.getInstance();
         ArrayList<String> arrayFinnKontaktPerson = new ArrayList<>();
         for (int i = 4 ; i< 10 ; i++){
-            arrayFinnKontaktPerson.add(array3.get(i));
+            arrayFinnKontaktPerson.add(linjeLestInn.get(i));
         }
         KontaktPerson kntktPerson = new KontaktPerson(arrayFinnKontaktPerson);
-        register.registrer(kntktPerson);
+        //register.registrer(kntktPerson);
+        System.out.print("funka2");
         return kntktPerson;
     }
     
-    public Artist FinnArtist(ArrayList<String> array3){
+    public Artist FinnArtist(ArrayList<String> linjeLestInn){
         //Artist NyArtist = new Artist();
+        Register register = Register.getInstance();
         ArrayList<String> arrayFinnArtist = new ArrayList<>();
         for (int i = 0 ; i< 4 ; i++){
             //array3.get(i) = array4.add(i);
-            arrayFinnArtist.add(array3.get(i));
+            arrayFinnArtist.add(linjeLestInn.get(i));
         }
         Artist NyArtist = new Artist(arrayFinnArtist);
-        register.registrer(NyArtist);
-        
+        //register.registrer(NyArtist);
+        System.out.print("funka1");
         return NyArtist;
     }
     
-    //private Register register=Register.getInstance();
-    public void lesFil (){
-        //ArrayList<Object> objekter=new ArrayList<>();
-        try(RandomAccessFile reader = new RandomAccessFile(vei, "r")) {
-        //ArrayList<String> array = new ArrayList<>();
-        String line = reader.readLine(); // skip meta data
+    public Arrangement FinnArrangement(ArrayList<String> linjeLestInn){
         
-        while((line = reader.readLine()) != null) {
-            String[] array1 = line.split(";"); // convertere til en Arraylist
-            //List<String> array2 = Arrays.asList(line.split(";"));
-            ArrayList<String> linjeLestInn = new ArrayList<>(Arrays.asList(line.split(";")));
-            for (int i = 0 ; i< linjeLestInn.size();i++){
-                System.out.print(linjeLestInn.get(i));
-            }
-            
+                    Artist artisten=FinnArtist(linjeLestInn);
+                    KontaktPerson kontaktPerson1 =finnKontaktPerson(linjeLestInn);
+                    
+                   
+                     String hentInnStringDato = linjeLestInn.get(15);
+                    String riktigFormat =hentInnStringDato.replace(".", "");
+                    //System.out.print(k);
+                    
+                   LocalDate dato =  LocalDate.parse(riktigFormat);
+                    int billettSalg = Integer.parseInt(linjeLestInn.get(16));
+                   
+                    
+                   Arrangement arrangement = new Arrangement(artisten, kontaktPerson1, linjeLestInn.get(10) , linjeLestInn.get(11) , linjeLestInn.get(12) , linjeLestInn.get(13) ,linjeLestInn.get(14), dato , billettSalg );
+                   //register.registrer(arrangement);
+                   return arrangement;
+    }
+    
+    
+    
+    public void tilFil(){
+        Register register = Register.getInstance();
+        //String csvFilNavn = "/Users/jonny/Documents/kll/Data.csv";
+       // String csvFilNavn = file.getPath();
+        String csvFilNavn=super.file.getPath();
+        System.out.println(csvFilNavn);
+        BufferedReader br = null;
+        String line = "";
+        //String cvsSplitBy = ";";
 
-//array=line.split(";");
-            switch(linjeLestInn.size()){
+        try {
+            br = new BufferedReader(new FileReader(csvFilNavn));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                //String[] arrayAvSplittetLinje = line.split(cvsSplitBy);
+                ArrayList<String> linjeLestInn = new ArrayList<>(Arrays.asList(line.split(";")));
+                if (linjeLestInn.size()!=4 )
+                System.out.print("funka :" + linjeLestInn.size());
+                
+                switch(linjeLestInn.size()){
                 case 4:
                     System.out.println("Artist");
-                    System.out.println(linjeLestInn.size()+"");
+                    
                     Artist artist=new Artist(linjeLestInn);
                     System.out.println(artist);
                     //objekter.add(artist);
@@ -84,26 +115,26 @@ public class FraCSV extends OpplastingSystem {
                     }
                     register.registrer(artist);
                     break;
-                case 7:
+                case 17:
                     System.out.println("Arrangement");
                     Artist artisten=FinnArtist(linjeLestInn);
                     KontaktPerson kontaktPerson1 =finnKontaktPerson(linjeLestInn);
                     
-                    ArrayList<String> arrangement1 = new ArrayList<>();
+                   /* ArrayList arrangement1 = new ArrayList<>();
                     for (int i = 10 ; i< linjeLestInn.size() ; i++ ){
                         arrangement1.add(linjeLestInn.get(i));
                     }
-                        
+                    */ //System.out.print("\n" + arrangement1.get(5) + "\n" +arrangement1.get(6));
+                    /*String k = linjeLestInn.get(15);
+                    String l =k.replace(".", "");
+                    System.out.print(k);
                     
-                    for (Artist a : register.getArtister()){
-                        if (a.equals(artisten)){
-                            System.out.println("samme fornavn!!!");
-                        }
-                        
-                    }
-                    
-                  // Arrangement arrangement = new Arrangement(artisten, kontaktPerson1,arrangement1  );
-                  // register.registrer(arrangement);
+                   LocalDate dato =  LocalDate.parse(l);
+                    int billettSalg = Integer.parseInt(linjeLestInn.get(16));*/
+                   
+                    Arrangement arrangement = FinnArrangement(linjeLestInn);
+                   //Arrangement arrangement = new Arrangement(artisten, kontaktPerson1, linjeLestInn.get(10) , linjeLestInn.get(11) , linjeLestInn.get(12) , linjeLestInn.get(13) ,linjeLestInn.get(14), dato , billettSalg );
+                   register.registrer(arrangement);
                    
                     break;
                 case 2:
@@ -117,15 +148,24 @@ public class FraCSV extends OpplastingSystem {
                     }
                     register.registrer(nyLokale);
                     break;
-                case 3:
+                case 19:
                     System.out.println("Billett");
+                    Arrangement arrangementIBillet = FinnArrangement(linjeLestInn);
+                   // String arrangementNavn = arrangementIBillet.getNavnPaaArrangement();
+                    int pris = Integer.parseInt(linjeLestInn.get(16));
                     //Billett billett = new Billett(array3);
-                    for (Billett a : register.getBillett()){
+                   /* for (Billett a : register.getBillett()){
                         if (a.equals(null)){
                             System.out.println("samme fornavn!!!");
                         }
                         
-                    }
+                    }*/
+                    register.registrer(new Billett(arrangementIBillet,pris, linjeLestInn.get(17)));
+                
+                    
+                    //register.registrer(billett);
+                    
+                   
                 
                     
                     //register.registrer(billett);
@@ -134,29 +174,45 @@ public class FraCSV extends OpplastingSystem {
                 case 6:
                     System.out.println("Kontakt person");
                     KontaktPerson kontaktPerson = new KontaktPerson(linjeLestInn);
-                    for (KontaktPerson a : register.getKontaktPerson()){
+                    /*for (KontaktPerson a : register.getKontaktPerson()){
                         if (a.equals(kontaktPerson)){
                             System.out.println("samme fornavn!!!");
-                        }
+                        }*/
                         
-                    }
+                    
                     register.registrer(kontaktPerson);
                    
                     break; 
+                
             }
-           
-            //register.registrer(objekter);
-        }
-        } catch (FileNotFoundException e) {
-                System.err.println("Could not find the specified file");
-        }   catch (IOException e) {
-                System.err.println("Could not read the specified file. Cause: " + e.getCause());
+                
+                
             }
+                
 
-    } 
+            
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     @Override
     public void lagreTilFil() throws ClassNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
+
+
+
+
